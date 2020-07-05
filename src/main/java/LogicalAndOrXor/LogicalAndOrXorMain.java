8495d27301b9745af05bbf063ec1ce1e8a62a8d2
shoaibshaikhcl <shoaib.shaikh@clairvoyantsoft.com>
2020-07-05 13:45:11 +0530

    refactored LogicalAndOrXor variables

--- src/main/java/LogicalAndOrXor/LogicalAndOrXorMain.java
+++ src/main/java/LogicalAndOrXor/LogicalAndOrXorMain.java
@@ -3,34 +3,34 @@ package LogicalAndOrXor;
 public class LogicalAndOrXorMain {
 
     public static void main(String[] args) {
+        final int MAX_INT = 50;
+        final int sInt7 = 7;
+        final int mInt16 = 16;
 
-            int s = 7;
-        int m = 16;
-
-        // if (s > m && m < 50)
-        if (Boolean.logicalAnd(s > m, m < 50)) {
+        // if (sInt7 > mInt16 && mInt16 < 50)
+        if (Boolean.logicalAnd(sInt7 > mInt16, mInt16 < MAX_INT)) {
             System.out.println("Boolean.logicalAnd returned true");
         } else {
             System.out.println("Boolean.logicalAnd returned false");
         }
 
-        // if (s > m || m < 50)
-        if (Boolean.logicalOr(s > m, m < 50)) {
+        // if (sInt7 > mInt16 || mInt16 < 50)
+        if (Boolean.logicalOr(sInt7 > mInt16, mInt16 < MAX_INT)) {
             System.out.println("Boolean.logicalOr returned true");
         } else {
             System.out.println("Boolean.logicalOr returned false");
         }
 
-        // if (s > m ^ m < 50)
-        if (Boolean.logicalXor(s > m, m < 50)) {
+        // if (sInt7 > mInt16 ^ mInt16 < 50)
+        if (Boolean.logicalXor(sInt7 > mInt16, mInt16 < MAX_INT)) {
             System.out.println("Boolean.logicalXor returned true");
         } else {
             System.out.println("Boolean.logicalXor returned false");
         }
 
         if (Boolean.logicalAnd(
-                Boolean.logicalOr(s > m, m < 50),
-                Boolean.logicalOr(s <= m, m > 50))) {
+                Boolean.logicalOr(sInt7 > mInt16, mInt16 < MAX_INT),
+                Boolean.logicalOr(sInt7 <= mInt16, mInt16 > MAX_INT))) {
             System.out.println("Combination returned true");
         } else {
             System.out.println("Combination returned false");
